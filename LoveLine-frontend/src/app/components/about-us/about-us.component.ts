992856c1b54debc46645a52af2bcc311e0f8b2c4
Elenaaaak <kekicelena@gmail.com>
2024-10-06 20:06:29 +0200

    cleaned some code and started the about us component

--- LoveLine-frontend/src/app/components/about-us/about-us.component.ts
+++ LoveLine-frontend/src/app/components/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-about-us',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
 
+  
+  ngOnInit(): void {
+    this.iconsMenuAnimations();
+  }
+
+  
+  iconsMenuAnimations(): void {
+    document.querySelectorAll('a').forEach(function (link) {
+      const svg = link.querySelector('svg');
+
+      link.addEventListener('mouseenter', function () {
+        gsap.to(svg, {
+          duration: 0.1,
+          opacity: 1,
+          scale: 1.1,
+          ease: 'power4.out',
+        });
+      });
+
+      link.addEventListener('mouseleave', function () {
+        gsap.to(svg, {
+          duration: 0.1,
+          opacity: 0,
+          scale: 0.5,
+          ease: 'power4.in',
+        });
+      });
+    });
+  }
+  
 }
